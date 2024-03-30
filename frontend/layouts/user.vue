a51a5808c97d9426974181f7a59b8524d5799d54
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-03-30 21:08:59 +0100

    Initialize signup page

--- frontend/layouts/user.vue
+++ frontend/layouts/user.vue
@@ -1,4 +1,15 @@
 <template>
-  <h1>Quanta</h1>
-  <slot />
+  <div class="flex flex-col gap-8">
+    <nav class="flex items-center justify-center bg-red-50 border border-red-200 h-40">
+      <h1 class="text-8xl text-white">Quanta</h1>
+    </nav>
+
+    <div class="bg-red-50 flex items-center justify-center p-4">
+      <div class="bg-red-100 border border-violet-600 rounded-lg p-8 flex justify-center flex-col">
+        <h2 class="text-4xl text-white mb-4">Create an Account!</h2>
+        
+        <base-input label="Username" />
+      </div>
+    </div>
+  </div>
 </template>
--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -5,5 +5,8 @@ definePageMeta({
 </script>
 
 <template>
-  <p>Rejestracja</p>
+  <div class="flex flex-col h-screen w-screen items-center">
+    <div class="h-16 w-16 bg-red-400" />
+    <div class="h-16 w-16 bg-red-400" />
+  </div>
 </template>
