9254132d0c900cf348922b94f379b5848c5d6f15
PLKuba <major.plays96@gmail.com>
2024-03-31 22:40:06 +0200

    change docker names, add new config version

--- backend/rest_api_service/database.py
+++ backend/rest_api_service/database.py
@@ -11,8 +11,11 @@ DATABASE_URL = config["PSQL"]["DATABASE_URL"]
 DATABASE = config["PSQL"]["DATABASE"]
 USER = config["PSQL"]["USER"]
 PASSWORD = config["PSQL"]["PASSWORD"]
+PORT = config["PSQL"]["PORT"]
+HOST = config["PSQL"]["HOST"]
 
-DATABASE_URL = DATABASE_URL.format(DATABASE, USER, PASSWORD)
+DATABASE_URL = DATABASE_URL.format(database=DATABASE, user=USER, password=PASSWORD, port=PORT, host=HOST)
+print(DATABASE_URL)
 
 _engine = create_engine(DATABASE_URL)
 
