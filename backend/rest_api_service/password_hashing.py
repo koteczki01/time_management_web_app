ac85ade135f36fa287d814a88a43aea465618d0a
piecharka <kowalas02.kk@gmail.com>
2024-04-12 18:11:39 +0200

    useless file deleted

--- backend/rest_api_service/password_hashing.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-
-def verify_password(hashed_password, user_password):
-  return bcrypt.checkpw(user_password.encode('utf-8'), hashed_password)
-
-password =  bcrypt.hashpw(b'busi', bcrypt.gensalt())
-
-print(verify_password(password, "busi"))
