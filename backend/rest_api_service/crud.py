b4180bfe8877340dfcac4b2a1d77fdefb9fd0c97
jacobkostek <92811093+jacobkostek@users.noreply.github.com>
2024-04-21 20:29:27 +0200

    123

--- backend/rest_api_service/crud.py
+++ backend/rest_api_service/crud.py
@@ -1,13 +1,75 @@
 from typing import Type
 import models
-from sqlalchemy.orm import Session
-from models import DBUser
+from sqlalchemy.orm import Session, joinedload
+from models import DBUser, DBUserFriendship
+from datetime import date
 
 
 async def get_all_users(db: Session) -> list[Type[DBUser]] | None:
     users = db.query(models.DBUser).all()
 
     return users
+
+
+
+ 
+async def get_user_by_id(db: Session, user_id: int) -> models.DBUser | None:
+    user = db.query(models.DBUser).filter(models.DBUser.user_id == user_id).first()
+    print(user)
+    
+    return user
+
+async def get_user_by_username(db: Session, username: str) -> models.DBUser | None:
+    user = db.query(models.DBUser).filter(models.DBUser.username == username).first()
+    print(user)
+    
+    return user
+
+async def get_all_events_by_user_id(db: Session, user_id: int) -> models.DBUser | None:
+    user = db.query(models.DBUser).options(joinedload(models.DBUser.events_created)).filter(models.DBUser.user_id == user_id).first()
+    return user
+
+
+async def get_all_friends_by_user_id(db: Session, user_id: int) -> models.DBUser | None:
+    user = db.query(models.DBUser).filter(models.DBUser.user_id == user_id).first()
+    if user:
+        return user.friends
+    return None
+
+async def create_user(db: Session, username: str, email: str, password_hash: str, birthday: date, is_active: bool = True):
+    new_user = DBUser(
+        username=username,
+        email=email,
+        password_hash=password_hash,
+        birthday=birthday,
+        created_at=date.today(),
+        last_login=date.today(),
+        update_date=date.today(),
+        is_active=is_active
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+async def delete_user(db: Session, user_id: int) -> bool:
+    user = await get_user_by_id(db, user_id)
+    if user:
+        # Usuń rekordy z tabeli DBUserFriendship powiązane z użytkownikiem
+        db.query(DBUserFriendship).filter(
+            (DBUserFriendship.user1_id == user_id) | (DBUserFriendship.user2_id == user_id)
+        ).delete(synchronize_session=False)
+        # Usuń użytkownika
+        db.delete(user)
+        db.commit()
+        return True
+    return False
+
+
+
+
+
+
 
 
 async def get_all_categories(db: Session) -> list[Type[models.DBCategory]] | None:
--- backend/rest_api_service/main.py
+++ backend/rest_api_service/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, status, Depends, Response
 import crud
 from database import SessionLocal
 from sqlalchemy.orm import Session
+from datetime import date
 
 
 app = FastAPI()
@@ -26,6 +27,81 @@ async def get_all_users(response: Response, db: Session = Depends(get_db)):
     except Exception as e:
         response.status_code = 500
         return {"message": f"An error occured: {e}"}
+
+
+
+
+
+@app.get("/users/get_user_by_id/{id}", tags = ['User'], status_code=status.HTTP_200_OK)
+async def get__user_by_id(id : int, response: Response, db: Session = Depends(get_db)):
+    try:
+        user = await crud.get_user_by_id(db, id)
+        print(id)
+        return user
+
+    except Exception as e:
+        response.status_code = 500
+        return {"message": f"An error occured: {e}"}
+
+@app.get("/users/get_user_by_username/{username}", tags = ['User'], status_code=status.HTTP_200_OK)
+async def get__user_by_username(username : str, response: Response, db: Session = Depends(get_db)):
+    try:
+        user = await crud.get_user_by_username(db, username)
+        print(username)
+        return user
+
+    except Exception as e:
+        response.status_code = 500
+        return {"message": f"An error occured: {e}"}
+    
+@app.get("/users/get_all_events_by_user_id/{user_id}", tags=['User'], status_code=status.HTTP_200_OK)
+async def get_all_events_by_user_id(user_id: int, response: Response, db: Session = Depends(get_db)):
+    try:
+        user = await crud.get_user_by_id(db, user_id)
+        if user:
+            events_created = user.events_created
+            return events_created
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "User not found"}
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"message": f"An error occurred: {e}"}
+
+@app.get("/users/get_all_friends_by_user_id/{user_id}", tags=['User'], status_code=status.HTTP_200_OK)
+async def get_all_friends_by_user_id(user_id: int, response: Response, db: Session = Depends(get_db)):
+    try:
+        user_friends = await crud.get_all_friends_by_user_id(db, user_id)
+        if user_friends:
+            return user_friends
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "User not found or user has no friends"}
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"message": f"An error occurred: {e}"}
+
+@app.post("/users/create_user", tags=['User'], status_code=status.HTTP_201_CREATED)
+async def create_user(response: Response, username: str, email: str, password_hash: str, birthday: date, is_active: bool = True, db: Session = Depends(get_db)):
+    try:
+        new_user = await crud.create_user(db, username, email, password_hash, birthday, is_active)
+        return new_user
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"message": f"An error occurred: {e}"}
+
+@app.delete("/users/delete_user/{user_id}", tags=['User'], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, response: Response, db: Session = Depends(get_db)):
+    try:
+        success = await crud.delete_user(db, user_id)
+        if success:
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "User not found"}
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"message": f"An error occurred: {e}"}
+
 
 
 @app.get("/category/get_all_categories", tags=['Category'], status_code=status.HTTP_200_OK)
