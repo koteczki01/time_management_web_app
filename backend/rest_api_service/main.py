0ba843ac9698f455fb1b4c0ed14aeb3ec42a675a
PC <PC@DESKTOP-ONMTGE3>
2024-04-23 19:53:40 +0200

    send friends request, accept friend request

--- backend/rest_api_service/main.py
+++ backend/rest_api_service/main.py
@@ -62,3 +62,25 @@ async def get_all_participants(response: Response, db: Session = Depends(get_db)
     except Exception as e:
         response.status_code = 500
         return {"message": f"An error occured: {e}"}
+       
+
+@app.post("/friend-request/{recipient_id}", tags=['Friend'])
+async def send_friend_request(recipient_id: int, db: Session = Depends(get_db)):
+    sender_id = 1  
+    friendship = DBUserFriendship(user1_id=sender_id, user2_id=recipient_id, friendship_status=friendship_status.pending)
+    db.add(friendship)
+    db.commit()
+    return {"message": "Friend request sent successfully"}
+
+@app.put("/friend-request/{request_id}/accept", tags=['Friend'])
+async def accept_friend_request(request_id: int, db: Session = Depends(get_db)):
+    friendship = db.query(DBUserFriendship).filter(DBUserFriendship.friendship_id == request_id).first()
+    if friendship:
+        if friendship.friendship_status == friendship_status.pending:
+            friendship.friendship_status = friendship_status.accepted
+            db.commit()
+            return {"message": "Friend request accepted successfully"}
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Friend request has already been accepted")
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Friend request not found")
