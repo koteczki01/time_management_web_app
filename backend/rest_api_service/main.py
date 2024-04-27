d82f2a0ce6f51dfc84aa6206a2730948dd378e00
eedyta <128971800+eedyta@users.noreply.github.com>
2024-04-27 11:20:42 +0200

    Update main.py

--- backend/rest_api_service/main.py
+++ backend/rest_api_service/main.py
@@ -1,44 +1,16 @@
 import datetime
 import bcrypt
-from fastapi import FastAPI, HTTPException, status, Depends, Response, Security
+from fastapi import FastAPI, HTTPException, status, Depends, Response
 import crud
 from database import SessionLocal
-from sqlalchemy.orm import sessionmaker,Session
+from sqlalchemy.orm import Session
 from schemas import *
 from schemas import UserLoginSchema, UserRegisterSchema
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from passlib.context import CryptContext
-import jwt
-import os
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declarative_base as db_base
-
 
 
 app = FastAPI()
 
-# Konfiguracja połączenia z bazą danych
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/Database"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = db_base()
-
-# Model danych użytkownika
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-
-Base.metadata.create_all(bind=engine)
-
-# Konfiguracja uwierzytelniania
-SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
-ALGORITHM = "HS256"
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Dependency
 def get_db():
@@ -47,19 +19,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict, expires_delta: datetime.timedelta):
-    to_encode = data.copy()
-    expire = datetime.datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @app.get("/users/get_all_users", tags=['User'], status_code=status.HTTP_200_OK)
@@ -149,26 +108,3 @@ async def register(user : UserRegisterSchema, response: Response, db: Session = 
         return {"message": f"An error occurred: {e}"}
 
 
-# Endpoint do rejestracji nowego użytkownika
-@app.post("/register/")
-def register_user(username: str, password: str, db: Session = Depends(get_db)):
-    hashed_password = get_password_hash(password)
-    db_user = User(username=username, hashed_password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    return {"message": "User registered successfully"}
-
-# Endpoint do generowania tokena JWT po poprawnym uwierzytelnieniu
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == form_data.username).first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
-    access_token_expires = datetime.timedelta(minutes=30)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
-
-# Endpoint wymagający autoryzacji
-@app.get("/protected")
-async def protected_route(token: str = Security(oauth2_scheme)):
-    return {"message": "You are authorized!"}
