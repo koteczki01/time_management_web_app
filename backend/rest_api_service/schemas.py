e63fd84b03d7888ed2a921bb002f64db276460e9
PLKuba <major.plays96@gmail.com>
2024-04-29 12:32:45 +0100

    add response models, refactor Exceptions, fix datetime bug

--- backend/rest_api_service/schemas.py
+++ backend/rest_api_service/schemas.py
@@ -1,17 +1,39 @@
 import datetime
 from pydantic import EmailStr, BaseModel
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.orm import Mapped
 
+
+class UserRegisterResponse(BaseModel):
+    user_id: int
+    birthday: datetime.datetime
+    last_login: datetime.datetime
+    username: str
+    email: EmailStr
+    created_at: datetime.datetime
+    update_date: datetime.datetime
+    is_active: bool
+
+
+class UserLoginResponse(BaseModel):
+    user_id: int
+    birthday: datetime.datetime
+    last_login: datetime.datetime
+    username: str
+    email: EmailStr
+    created_at: datetime.datetime
+    update_date: datetime.datetime
+    is_active: bool
 
 
 class UserLoginSchema(BaseModel):
-  username : str
-  password_hash : str
+    username : str
+    password_hash : str
 
 
 class UserRegisterSchema(BaseModel):
-  username : str
-  password : str
-  email : EmailStr
-  birthday : datetime.datetime
+    username : str
+    password : str
+    email : EmailStr
+    birthday : datetime.datetime
+
+class ErrorOccured(BaseModel):
+    message: str
