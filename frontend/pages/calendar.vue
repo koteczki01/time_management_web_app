3f45d0236e474ed4a4778c6765769c3fedbd7ef9
Natalia Konopka <natalia_konopka213@wp.pl>
2024-05-04 18:46:55 +0200

    Kafelki użytkowników

--- frontend/pages/calendar.vue
+++ frontend/pages/calendar.vue
@@ -11,7 +11,7 @@
   </div>
   <div class="calendar-bar">
     <div class="friends">
-    <h1>Friends</h1>
+      <h1>Friends</h1>
     </div>
     <div>
     <h1>Monday</h1>
@@ -35,6 +35,11 @@
     <h1>Sunday</h1>
     </div>
   </div>
+  <div class="friends_names">
+    <div class="name" v-for="friend in numberOfFriends" :key="friend">
+      Natalia
+    </div>
+  </div>
 
 </template>
 
@@ -42,12 +47,9 @@
 body{
   margin: 0;
 }
-.friends{
-  background-color: #FFE8E8;
-}
 .top-bar {
   width: 100%;
-  height: 80px;
+  height: 13vh;
   background-color: #FFE8E8;
   display: flex;
   justify-content: center;
@@ -68,7 +70,7 @@ body{
 .left-top-bar{
   font-size: 1.7rem;
   width: 100%;
-  height: 50px;
+  height: 10%;
   background-color: #FFE8E8;
   display: flex;
   font-family: 'Lao Muang Don', sans-serif;
@@ -77,7 +79,7 @@ body{
 }
 .right-top-bar{
   width: 100%;
-  height: 50px;
+  height: 10%;
   background-color: #FFE8E8;
   display: flex;
   font-family: 'Lao Muang Don', sans-serif;
@@ -87,45 +89,80 @@ body{
   margin-right: 30px;
 }
 .plus {
-    margin-right: 20px;
-    background-color: #9E7E9B;
-    width: 40px;
-    height: 40px;
-    border-radius: 25px;
-    border: 0px;
-    color: white;
-    cursor: pointer;
-    -webkit-text-stroke: 1x rgb(85, 68, 76);
-    text-shadow: 2px 2px 2px rgb(85, 68, 76, 0.6);
-    font-size: 1.5rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  margin-right: 20px;
+  background-color: #9E7E9B;
+  width: 40px;
+  height: 40px;
+  border-radius: 25px;
+  border: 0px;
+  color: white;
+  cursor: pointer;
+  -webkit-text-stroke: 1px rgb(85, 68, 76);
+  text-shadow: 2px 2px 2px rgb(85, 68, 76, 0.6);
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .calendar-bar{
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    align-items: flex-start;
-    justify-content: center;
-    border-bottom: 2px solid #CCB2C6;
-    border-bottom: none;
+  display: flex;
+  align-items: flex-start;
 }
-.calendar-bar div{
-    border-right: 2px solid #CCB2C6;
-    border-bottom: none;
-    margin: 0px;
-    width: 12.5%;
-    display: flex;
-    justify-content: center;
-    height: 100%;
+.calendar-bar div:not(.friends) {
+  border-right: 2px solid #CCB2C6;
+  border-bottom: none;
+  display: flex;
+  justify-content: center;
+  width: 12.4%;
 }
 .calendar-bar div h1{
-    -webkit-text-stroke: 1px rgb(85, 68, 76);
-    text-shadow: 2px 2px 2px rgb(85, 68, 76, 0.6);
-    font-family: 'Lao Muang Don', sans-serif;
-    color: #fff;
-    font-size: 1.7rem;
+  -webkit-text-stroke: 1px rgb(85, 68, 76);
+  text-shadow: 2px 2px 2px rgb(85, 68, 76, 0.6);
+  font-family: 'Lao Muang Don', sans-serif;
+  color: #fff;
+  font-size: 1.7rem;
 }
-
+.friends{
+  background-color: #FFE8E8;
+  width:12.5%;
+  border-right: 2px solid #CCB2C6;
+  border-bottom: none;
+  display: flex;
+  justify-content: center;
+}
+.friends_names {
+  border-right: 2px solid #CCB2C6;
+  width: 12.5%;
+  color: white;
+  -webkit-text-stroke: 1px rgb(85, 68, 76);
+  text-shadow: 2px 2px 2px rgb(85, 68, 76, 0.6);
+  font-family: 'Lao Muang Don', sans-serif;
+  background-color: #FFE8E8;
+  padding: 0;
+  margin: 0;
+}
+.name {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding-bottom: 5px;
+  margin-bottom: 10px;
+  height: 33.33vh;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.1); 
+  font-size: 1.6rem;
+  background-color: #cfb2d4;
+}
 </style>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    data() {
+      return {
+        numberOfFriends: 3
+      };
+    }
+  });
+</script>
