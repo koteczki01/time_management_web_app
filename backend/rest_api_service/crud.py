0b55468733b5db36dc7381fa8f5cafc30448a7da
Sobiesky <34169914+PLKuba@users.noreply.github.com>
2024-05-23 10:20:04 +0200

    Update crud.py

--- backend/rest_api_service/crud.py
+++ backend/rest_api_service/crud.py
@@ -5,31 +5,6 @@ from sqlalchemy.orm import Session
 from models import DBUser
 from sqlalchemy.orm.exc import NoResultFound
 
-
-async def get_all_users(db: Session) -> list[Type[DBUser]] | None:
-    users = db.query(models.DBUser).all()
-
-    return users
-
-
-async def get_all_categories(db: Session) -> list[Type[models.DBCategory]] | None:
-    categories = db.query(models.DBCategory).all()
-
-    return categories
-
-
-async def get_all_events(db: Session) -> list[Type[models.DBEvent]] | None:
-    events = db.query(models.DBEvent).all()
-
-    return events
-
-
-async def get_all_participants(db: Session) -> list[Type[models.DBEventParticipants]] | None:
-    participants = db.query(models.DBEventParticipants).all()
-
-    return participants
-
-
 async def get_user_by_id(db: Session, user_id: int) -> Type[models.DBUser] | None:
     try:
         user = db.query(models.DBUser).filter(models.DBUser.user_id == user_id).first()
@@ -82,16 +57,14 @@ async def update_user_last_login(db: Session, id : int) -> None:
         db.rollback()  
         return None
 
-async def create_friend_request(db: Session, sender_id: int, recipient_id: int) -> DBUserFriendship:
-    friendship = DBUserFriendship(user1_id=sender_id, user2_id=recipient_id, friendship_status=FriendshipStatus.pending)
+async def create_friend_request(db: Session, sender_id: int, recipient_id: int) -> models.DBUserFriendship:
+    friendship = models.DBUserFriendship(user1_id=sender_id, user2_id=recipient_id, friendship_status=FriendshipStatus.pending)
     db.add(friendship)
     db.commit()
     return friendship
 
-async def get_friend_request(db: Session, sender_id: int) -> DBUserFriendship:
-    friendship = db.query(DBUserFriendship).filter(
-        (DBUserFriendship.user1_id == sender_id) | (DBUserFriendship.user2_id == sender_id)
-    ).filter_by(friendship_status=FriendshipStatus.pending).first()
+async def get_friend_request(db: Session, sender_id: int) -> models.DBUserFriendship:
+    friendship = db.query(models.DBUserFriendship).filter(
+        (models.DBUserFriendship.user1_id == sender_id) | (models.DBUserFriendship.user2_id == sender_id)
+    ).filter_by(models.DBUserFriendship.friendship_status=models.DBUserFriendship.FriendshipStatus.pending).first()
     return friendship
-
-
